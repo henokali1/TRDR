9c4290970b0f23d622f541836b196c421e6028d3
henok ali <henokali1@gmail.com>
2020-07-23 07:22:08 +0400

    csv

--- pg.py
+++ pg.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+import csv
 
 def time_price(val):
     ts = int(str(val[0])[:-3])
@@ -20,7 +21,46 @@ def action(c_pd, f_pd):
         return 'S'
     if((c_pd > 0) and (f_pd > 0)):
         return 'H'
-    
+
+def format_data(json_data):
+    pp = 0.0
+    pd_lst = []
+    frmtd_data = [["Time", "Price", "% Diff", "Action", "Trade"]]
+    for i, val in enumerate(json_data):
+        tp = time_price(val)
+        tim = tp['time']
+        cp = tp['price']
+        if i == 0:
+            pd = 0
+            pp = cp
+        else:
+            pd =  percentage_diff(cp, pp)
+            pp = cp
+            
+
+        pd_lst.append([tim, cp, pd])
+
+    for i, val in enumerate(pd_lst):
+        if (i == len(pd_lst) - 1) or (i == 0):
+            a=val
+            a.append('H')
+            frmtd_data.append(a)
+        else:
+            c_pd = float(val[2])
+            f_pd = float(pd_lst[i+1][2])
+            act = action(c_pd, f_pd)  
+            a = val
+            a.append(act)             
+            frmtd_data.append(a)
+    return frmtd_data
+
+def save_csv(file_name, data_list):
+    print(data_list[0])
+    print(data_list[1])
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter='\t')
+        writer.writerows(data_list)
+
 start_date = '2020-06-27'
 end_date = '2020-06-28'
 url = 'https://production.api.coindesk.com/v2/price/values/BTC?start_date={}T20:00&end_date={}T19:59&ohlc=false'.format(start_date, end_date)
@@ -29,5 +69,16 @@ resp = requests.get(url=url)
 d = resp.json()
 
 msg = d['message']
-data = d['data']['entries']
+if msg == 'OK':
+    data = d['data']['entries']
+    ll = format_data(data)
+    file_name = start_date + '--' + end_date + '.csv'
+    save_csv(file_name, ll)
+else:
+    print('Err: Couldn\'t get json data')
 
+
+data_list = [["SN", "Name", "Contribution"],
+             [6, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
