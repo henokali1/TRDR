9164f71aba0cd6eacb6a7c37f167048eae9b6cf1
henokali1 <henokali1@gmail.com>
2020-07-26 14:44:44 +0000

    .

--- v4-price_pd-volume_pd-volume-price-mins_bn_trade.py
+++ v4-price_pd-volume_pd-volume-price-mins_bn_trade.py
@@ -15,24 +15,20 @@ def timestamp_to_time(val):
 	return(tim[:-3])
 
 def percentage_diff(cp, pp):
-    return round((100.0*(cp-pp))/pp,4)
-
-def min_bn_trades(inital_trade, current_trade):
-	return int((current_trade-inital_trade)/60)
+	try:
+		return (100.0*(cp-pp))/pp
+	except:
+		return 0.0
 
 def action(c_pd, f_pd):
-	# if((c_pd < 0) and (f_pd < 0)):
-	# 	return 'H'
-	# if((c_pd < 0) and (f_pd > 0)):
-	# 	return 'B'
-	# if((c_pd > 0) and (f_pd < 0)):
-	# 	return 'S'
-	# if((c_pd > 0) and (f_pd > 0)):
-	# 	return 'H'
-	# else:
-	# 	return 'H'
+	if((c_pd < 0) and (f_pd < 0)):
+		return 'H'
 	if((c_pd < 0) and (f_pd > 0)):
 		return 'B'
+	if((c_pd > 0) and (f_pd < 0)):
+		return 'S'
+	if((c_pd > 0) and (f_pd > 0)):
+		return 'H'
 	else:
 		return 'H'
 
@@ -40,26 +36,24 @@ def priv_price(val):
 	sp = val.split(',')
 	return float(sp[1])
 
-def priv_vol(val):
-	sp = val.split(',')
-	return float(sp[-1])
-
 def write_csv(val, file_name):
 	try:
+		print('Exporting file.....')
 		with open(file_name, 'w') as the_file:
 			the_file.write(val)
 			print(file_name + ' exported')
 	except:
 		print("Couldn't export data :'(")
 
-def chunk_data(pd_lst, price_lst, act_lst, volume_lst, export_file_name):
+
+def chunk_data(price_lst, volume_lst, price_pd_lst, volume_pd_lst, price_pd_bn_trades_lst, volume_pd_bn_trades_lst, mins_bn_trade_lst, act_lst, export_file_name):
 	r=''
 
-	title = 'price_pd, volume_pd, price_pd_bn_trades, volume_pd_bn_trades, mins_bn_trade, volume, price\n'
+	title = 'Price,Volume,Price_PD,Volume_PD,Price_PD_bn_Trades,Volume_PD_bn_Trades,Mins_bn_Trade,Action\n'
 	r += title
-	for i,val in enumerate(pd_lst):
-		if(i <= len(pd_lst)):
-			r += '{},{},{},{}\n'.format(price_lst[i], volume_lst[i], pd_lst[i], act_lst[i])
+	for i,val in enumerate(price_pd_lst):
+		if(i <= len(price_pd_lst)):
+			r += '{},{},{},{},{},{},{},{}\n'.format(price_lst[i], volume_lst[i], price_pd_lst[i], volume_pd_lst[i], price_pd_bn_trades_lst[i], volume_pd_bn_trades_lst[i], mins_bn_trade_lst[i], act_lst[i])
 	write_csv(r, file_name=export_file_name)
 	
 	
@@ -70,7 +64,7 @@ def prepare_dataset(raw_data_file_name, size, update, export_file_name):
 
 	spltd = rd.split('\n')
 	del spltd[-1]
-	del spltd[0]
+	# del spltd[0]
 	data_sp = spltd[size:]
 
 	price_pd_lst = []
@@ -78,60 +72,71 @@ def prepare_dataset(raw_data_file_name, size, update, export_file_name):
 	price_lst = []
 	volume_lst = []
 	act_lst = []
-
-	priv_trade_price_idx = 0
+	price_pd_bn_trades_lst = []
+	volume_pd_bn_trades_lst = []
+	mins_bn_trade_lst = []
 	priv_trade_price = 0.0
-	priv_trade_volume_idx = 0
-	priv_trade_volume = 0.0
-	priv_action = ''
+	priv_trade_vol = 0.0
+	mins_bn_trade = 0
+	priv_trade_ts = 0
+	priv_act = ''
 	for i, val in enumerate(data_sp):
 		sp = val.split(',')
 		open_price = float(sp[1])
 		volume = float(sp[-1])
+		cur_ts = int(sp[0])
 
 		pp = open_price if i == 0 else priv_price(data_sp[i-1])
-		pv = volume if i == 0 else priv_vol(data_sp[i-1])
-
+		pv = volume if i == 0 else float(data_sp[i-1].split(',')[-1])
 		cp = open_price
-		c_pd = percentage_diff(cp, pp)
-		vol_pd = percentage_diff(volume, pv)
+		cv = volume
+		c_pd = round(percentage_diff(cp, pp), 4)
+		c_vd = round(percentage_diff(cv, pv), 4)
 		fp = cp if i == len(data_sp) -1 else priv_price(data_sp[i+1])
 
 		f_pd = percentage_diff(fp, cp)
 		act = action(c_pd, f_pd)
-
-		if priv_action != act:
-			price_pd_bn_trades = percentage_diff(cp, priv_price(priv_trade_price)
-			volume_pd_bn_trades = percentage_diff(volume, priv_price(data_sp[priv_trade_volume_idx]))
-
+		mins_bn_trade = int((cur_ts-priv_trade_ts)/60)
+		if priv_act != act:
+			price_pd_bn_trades = round(percentage_diff(cp, priv_trade_price), 4)
+			volume_pd_bn_trades = round(percentage_diff(cv, priv_trade_vol), 4)
 			priv_trade_price = cp
-			priv_trade_volume = volume
-
-			priv_trade_price_idx = i
-			priv_trade_volume_idx = i
+			priv_trade_vol = volume
+			priv_trade_ts = cur_ts
 		else:
 			price_pd_bn_trades = 0.0
 			volume_pd_bn_trades = 0.0
-		print(sp, act)
-		print(price_pd_bn_trades, volume_pd_bn_trades, i, priv_trade_price_idx)
-		priv_action = act
 
-		
+
 		price_pd_lst.append(c_pd)
-		volume_pd_lst.append(c_pd)
+		volume_pd_lst.append(c_vd)
 		price_lst.append(open_price)
 		volume_lst.append(volume)
+		price_pd_bn_trades_lst.append(price_pd_bn_trades)
+		volume_pd_bn_trades_lst.append(volume_pd_bn_trades)
+		mins_bn_trade_lst.append(mins_bn_trade)
 		act_lst.append(act)
-		if(i%update == 0):
-			# print("Remaining:\t",-1*size-i)
-			pass
-	# chunk_data(pd_lst=pd_lst, price_lst=price_lst, act_lst=act_lst, volume_lst=volume_lst, export_file_name=export_file_name)
+		# if(i%update == 0):
+		# 	print("Remaining:\t",-1*size-i)
+	     
+	chunk_data(
+		price_lst=price_lst,
+		volume_lst=volume_lst,
+		price_pd_lst=price_pd_lst,
+		volume_pd_lst=volume_pd_lst,
+		price_pd_bn_trades_lst=price_pd_bn_trades_lst,
+		volume_pd_bn_trades_lst=volume_pd_bn_trades_lst,
+		mins_bn_trade_lst=mins_bn_trade_lst,
+		act_lst=act_lst,
+		export_file_name=export_file_name,
+	)
 
 # size = 1534866
 # update = int(input('Update: '))
-size = 10
-update = 1
 
 # exp_fn = input('Exprot Dataset Filename: ')+'.csv'
-exp_fn = 'v4-BH-price_pd-volume_pd-volume-price-mins_bn_trade.csv'
+size = 10
+update = 1
+exp_fn = 'test.csv'
+
 prepare_dataset(raw_data_file_name='raw_data.csv', export_file_name=exp_fn, size=size, update=update)
