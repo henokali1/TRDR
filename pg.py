14190ce6d2902ec48af2cdb8dfed34f4afded7a7
henok ali <henokali1@gmail.com>
2020-07-28 23:38:21 +0400

    2 day data testing

--- pg.py
+++ pg.py
@@ -1,7 +1,7 @@
 import requests
 import ast
 
-wrong = []
+
 
 def read_file(fn):
 	with open(fn,'r') as f:
@@ -11,33 +11,8 @@ def read_file(fn):
 	del sp[-1]
 	return sp
 
-def pred(payload, actual_action, idx):
-	r = requests.post("http://34.83.91.155:8080/predict", json=payload).text
-	p = ast.literal_eval(r)
-	row_pred = p['predictions'][0]
-	scores = row_pred['scores']
-	classes = row_pred['classes']
-	confid_score = max(scores)
-	pred_index = scores.index(confid_score)
-	prediction = classes[pred_index]
-
-	if not (prediction == actual_action):
-		wrong.append(idx)
-	# print(prediction == actual_action, actual_action, prediction)
-
-
-lst = read_file('v5-testing-dataset-all.csv')
-
-
-for i, d in enumerate(lst):
-	# d = input("Data: ")
-	# if (d == 'q' or d == 'Q'):
-	# 	break
-	# else:
-	d = d.replace('\n','')
-	sp = d.split(',')
-	actual_action = sp[-1]
-
+def pay_load_format(val):
+	sp = val.split(',')
 	payload = {
 		"instances": [
 			{
@@ -55,9 +30,55 @@ for i, d in enumerate(lst):
 			}
 		]
 	}
+	
+	return(payload)
 
-	pred(payload, actual_action, i)
-	if i%100000 == 0:
-		print('Remaining:\t',(1534866 - i))
-print(len(wrong))
+
+def pred(payload):
+	r = requests.post("http://34.83.91.155:8080/predict", json=payload).text
+	p = ast.literal_eval(r)
+	row_pred = p['predictions'][0]
+	scores = row_pred['scores']
+	classes = row_pred['classes']
+	confid_score = max(scores)
+	pred_index = scores.index(confid_score)
+	prediction = classes[pred_index]
+	return prediction
+
+def inp_pred():
+	while 1:
+		d = input("Data: ")
+		if (d == 'q' or d == 'Q'):
+			break
+		else:
+			d = d.replace('\n','')
+			sp = d.split(',')
+			actual_action = sp[-1]
+
+			payload = pay_load_format(d)
+
+			pred(payload, actual_action, 3)
+
+lst = read_file('t.csv')
+wrong = []
+for idx,i in enumerate(lst):
+	if (idx%100) == 0:
+		print('Remaining: {}'.format(len(lst)-idx))
+		w = len(wrong)
+		t = len(lst)
+		accuracy = round(100 - (w*100.0)/t, 2)
+		print("Accuracy: {}%".format(accuracy))
+	sp = i.split(',')
+	actual_action = sp[-1]
+	payload = pay_load_format(i)
+	prediction = pred(payload)
+
+	if (prediction != actual_action):
+		wrong.append(idx)
+	# print(prediction == actual_action, actual_action, prediction)
+
+
+
 print(wrong)
+
+print("Accuracy: {}%".format(accuracy))
