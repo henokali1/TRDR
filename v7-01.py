7d7c3682ebc1361189ad3a2a38a2249b778a25c3
henokali1 <henokali1@gmail.com>
2020-08-07 15:30:10 +0400

    v7.01

--- v7-01.py
+++ v7-01.py
@@ -4,3 +4,24 @@ b_prev_pattern={'H': {'pattern': 'H', 'cntr': 6185}, 'S': {'pattern': 'S', 'cntr
 b_prev_prev_pattern={'S': {'pattern': 'S', 'cntr': 6185}, 'H': {'pattern': 'H', 'cntr': 6237}, 'H-H': {'pattern': 'H-H', 'cntr': 3237}, 'B': {'pattern': 'B', 'cntr': 6175}, 'H-H-H': {'pattern': 'H-H-H', 'cntr': 1562}, 'H-H-H-H': {'pattern': 'H-H-H-H', 'cntr': 799}, 'H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H', 'cntr': 199}, 'H-H-H-H-H': {'pattern': 'H-H-H-H-H', 'cntr': 382}, 'H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H', 'cntr': 87}, 'H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H', 'cntr': 39}, 'H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H', 'cntr': 37}, 'H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H', 'cntr': 6}, 'H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 1}, 'H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H', 'cntr': 9}}
 h_prev_pattern={'H': {'pattern': 'H', 'cntr': 12623}, 'S': {'pattern': 'S', 'cntr': 12571}, 'B': {'pattern': 'B', 'cntr': 12348}, 'H-H': {'pattern': 'H-H', 'cntr': 6297}, 'H-H-H': {'pattern': 'H-H-H', 'cntr': 3163}, 'H-H-H-H': {'pattern': 'H-H-H-H', 'cntr': 1598}, 'H-H-H-H-H': {'pattern': 'H-H-H-H-H', 'cntr': 769}, 'H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H', 'cntr': 387}, 'H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H', 'cntr': 187}, 'H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H', 'cntr': 94}, 'H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H', 'cntr': 46}, 'H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H', 'cntr': 18}, 'H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H', 'cntr': 8}, 'H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 3}, 'H-H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 1}, 'H-H-H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 1}, 'H-H-H-H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 1}, 'H-H-H-H-H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 1}}
 h_prev_prev_pattern={'': {'pattern': '', 'cntr': 1}, 'H-H': {'pattern': 'H-H', 'cntr': 6244}, 'H': {'pattern': 'H', 'cntr': 12339}, 'S': {'pattern': 'S', 'cntr': 12561}, 'B': {'pattern': 'B', 'cntr': 12668}, 'H-H-H': {'pattern': 'H-H-H', 'cntr': 3159}, 'H-H-H-H': {'pattern': 'H-H-H-H', 'cntr': 1562}, 'H-H-H-H-H': {'pattern': 'H-H-H-H-H', 'cntr': 816}, 'H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H', 'cntr': 212}, 'H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H', 'cntr': 384}, 'H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H', 'cntr': 95}, 'H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H', 'cntr': 34}, 'H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H', 'cntr': 8}, 'H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H', 'cntr': 24}, 'H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 4}, 'H-H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 2}, 'H-H-H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 1}, 'H-H-H-H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 1}, 'H-H-H-H-H-H-H-H-H-H-H-H-H-H-H-H-H': {'pattern': 'H-H-H-H-H-H-H-H-H-H-H-H-H-H-H-H-H', 'cntr': 1}}
+
+# Python3 code to demonstrate working of 
+# Get Unique values from list of dictionary 
+# Using set() + values() + dictionary comprehension 
+
+# Initialize list 
+test_list = [{'gfg' : 1, 'is' : 2}, {'best' : 1, 'for' : 3}, {'CS' : 2}] 
+
+# printing original list 
+# print("The original list : " + str(test_list)) 
+
+# Using set() + values() + dictionary comprehension 
+# Get Unique values from list of dictionary 
+def unique_vals(x,y):
+    test_list = [{**x, **y}]
+    return(list(set(val for dic in test_list for val in dic.values())) )
+	
+# printing result 
+res = unique_vals(s_prev_pattern,h_prev_pattern)
+print("Total unique vals: ", len(res))
+print("The unique values in list are : " + str(res)) 
