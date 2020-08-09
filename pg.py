7f38231afd88e0a18da63d3ddd03ee5046b3b100
henokali1 <henokali1@gmail.com>
2020-08-09 02:27:42 +0000

    .

--- pg.py
+++ pg.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import inflect
+p = inflect.engine()
 
 def time_price(val):
 	ts = int(str(val[0])[:-3])
@@ -39,36 +41,57 @@ def write_csv(val, file_name):
 	except:
 		print("Couldn't export data :'(")
 
-def chunk_data(pd_lst, price_lst, act_lst, volume_lst, export_file_name):
+def chunk_data(time_lst, pd_lst, price_lst, act_lst, late_act, export_file_name):
 	r=''
 
-	title = 'Price,' + 'Volume,' + 'Pd' + ',Action\n'
+	title = 'Time,' + 'Price,' + 'Pd,' + 'Action\n'
 	r += title
 	for i,val in enumerate(pd_lst):
 		if(i <= len(pd_lst)):
-			r += '{},{},{},{}\n'.format(price_lst[i], volume_lst[i], pd_lst[i], act_lst[i])
+			r += f'{time_lst[i]},{price_lst[i]},{pd_lst[i]},{act_lst[i]},{late_act[i]}\n'
 	write_csv(r, file_name=export_file_name)
 	
 	
-def prepare_dataset(raw_data_file_name, update, export_file_name):
+def prepare_dataset(raw_data_file_name, size, update, export_file_name):
+	size = -1*size
 	with open(raw_data_file_name,'r') as f:
 		rd = f.read()
-	sp = rd.split('\n')[3:-1]
-	tot = len(sp)
-	r='Time,Price\n'
-	for i,ln in enumerate(sp):
-		spp = ln.split(',')
-		r+=spp[0]+','+spp[16]+'\n'
 
+	spltd = rd.split('\n')
+	del spltd[-1]
+	# del spltd[0]
+	data_sp = spltd[size:]
+
+	pd_lst = []
+	price_lst = []
+	act_lst = []
+	time_lst = []
+	for i, val in enumerate(data_sp):
+		sp = val.split(',')
+		open_price = float(sp[1])
+		tm = sp[0]
+		pp = open_price if i == 0 else priv_price(data_sp[i-1])
+
+		cp = open_price
+		c_pd = percentage_diff(cp, pp)
+
+		fp = cp if i == len(data_sp) -1 else priv_price(data_sp[i+1])
+
+		f_pd = percentage_diff(fp, cp)
+		act = action(c_pd, f_pd)
+
+		time_lst.append(tm)
+		pd_lst.append(c_pd)
+		price_lst.append(open_price)
+		act_lst.append(act)
 		if(i%update == 0):
-			print("Remaining:\t",tot-i)
-	print('Exporting ',export_file_name,'........')
-	write_csv(r, file_name=export_file_name)
+			print("Remaining:\t",-1*size-i)
+	late_act = ['H'] + act_lst[:-1]
+	chunk_data(time_lst=time_lst, pd_lst=pd_lst, price_lst=price_lst, act_lst=act_lst, late_act=late_act, export_file_name=export_file_name)
 
-
-	
+size = 42959
 update = 1000
 
-exp_fn = 'aapl'+'.csv'
+exp_fn = 'v8.csv'
 
-prepare_dataset(raw_data_file_name='dataset.csv', export_file_name=exp_fn, update=update)
+prepare_dataset(raw_data_file_name='aapl.csv', export_file_name=exp_fn, size=size, update=update)
