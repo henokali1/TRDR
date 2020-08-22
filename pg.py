34d35071063dad2cf603a13c2904b00eb18b62b4
henokali1 <henokali1@gmail.com>
2020-08-22 12:49:30 +0000

    .

--- pg.py
+++ pg.py
@@ -16,16 +16,16 @@ def pay_load_format(val):
 	payload = {
 		"instances": [
 			{
-				"PricePD": -0.013926689,
-				"PricePdBP": 0.019439075,
+				"PricePD": -0.0012622105724754,
+				"PricePdBP": 0.014307280406922,
 				"trend_lst": "D",
-				"TrendHist": "D-D",
-				"TrendHistDownCount": 2,
+				"TrendHist": "D",
+				"TrendHistDownCount": 1,
 				"TrendHistUpCount": 0,
-				"TwoTrendHist": "D-D-D",
-				"TwoTrendHistDownCount": 3,
-				"TwoTrendHistUpCount": 0,
-				"nw_act_patt_lst": "D"
+				"TwoTrendHist": "U-D",
+				"TwoTrendHistDownCount": 1,
+				"TwoTrendHistUpCount": 1,
+				"nw_act_patt_lst": "U"
 			}
 		]
 	}
@@ -37,13 +37,13 @@ def pred(payload):
 	r = requests.post("http://34.83.91.155:8080/predict", json=payload).text
 	# r = requests.post("http://localhost:8080/predict", json=payload).text
 	p = ast.literal_eval(r)
-	# row_pred = p['predictions'][0]
-	# scores = row_pred['scores']
-	# classes = row_pred['classes']
-	# confid_score = max(scores)
-	# pred_index = scores.index(confid_score)
-	# prediction = classes[pred_index]
-	return r
+	row_pred = p['predictions'][0]
+	scores = row_pred['scores']
+	classes = row_pred['classes']
+	confid_score = max(scores)
+	pred_index = scores.index(confid_score)
+	prediction = classes[pred_index]
+	return prediction
 
 payload = pay_load_format('d')
 a = pred(payload)
