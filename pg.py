e45fdedac799cac0b2dfb0e519e33381d3ce3ff4
henokali1 <henokali1@gmail.com>
2020-08-22 11:46:15 +0000

    .

--- pg.py
+++ pg.py
@@ -25,7 +25,7 @@ def pay_load_format(val):
 				"TwoTrendHist": "D-D-D",
 				"TwoTrendHistDownCount": 3,
 				"TwoTrendHistUpCount": 0,
-				"nw_act_patt_lst": D
+				"nw_act_patt_lst": "D"
 			}
 		]
 	}
@@ -34,51 +34,54 @@ def pay_load_format(val):
 
 
 def pred(payload):
-	# r = requests.post("http://34.83.91.155:8080/predict", json=payload).text
-	r = requests.post("http://localhost:8080/predict", json=payload).text
+	r = requests.post("http://34.83.91.155:8080/predict", json=payload).text
+	# r = requests.post("http://localhost:8080/predict", json=payload).text
 	p = ast.literal_eval(r)
-	row_pred = p['predictions'][0]
-	scores = row_pred['scores']
-	classes = row_pred['classes']
-	confid_score = max(scores)
-	pred_index = scores.index(confid_score)
-	prediction = classes[pred_index]
-	return prediction
+	# row_pred = p['predictions'][0]
+	# scores = row_pred['scores']
+	# classes = row_pred['classes']
+	# confid_score = max(scores)
+	# pred_index = scores.index(confid_score)
+	# prediction = classes[pred_index]
+	return r
 
-def inp_pred():
-	while 1:
-		d = input("Data: ")
-		if (d == 'q' or d == 'Q'):
-			break
-		else:
-			d = d.replace('\n','')
-			sp = d.split(',')
-			actual_action = sp[-1]
+payload = pay_load_format('d')
+a = pred(payload)
+print(a)
+# def inp_pred():
+# 	while 1:
+# 		d = input("Data: ")
+# 		if (d == 'q' or d == 'Q'):
+# 			break
+# 		else:
+# 			d = d.replace('\n','')
+# 			sp = d.split(',')
+# 			actual_action = sp[-1]
 
-			payload = pay_load_format(d)
+# 			payload = pay_load_format(d)
 
-			pred(payload, actual_action, 3)
+# 			pred(payload, actual_action, 3)
 
-lst = read_file('t.csv')
-wrong = []
-for idx,i in enumerate(lst):
-	if (idx%100) == 0:
-		print('Remaining: {}'.format(len(lst)-idx))
-		w = len(wrong)
-		t = len(lst)
-		accuracy = round(100 - (w*100.0)/t, 2)
-		print("Accuracy: {}%".format(accuracy))
-	sp = i.split(',')
-	actual_action = sp[-1]
-	payload = pay_load_format(i)
-	prediction = pred(payload)
+# lst = read_file('t.csv')
+# wrong = []
+# for idx,i in enumerate(lst):
+# 	if (idx%100) == 0:
+# 		print('Remaining: {}'.format(len(lst)-idx))
+# 		w = len(wrong)
+# 		t = len(lst)
+# 		accuracy = round(100 - (w*100.0)/t, 2)
+# 		print("Accuracy: {}%".format(accuracy))
+# 	sp = i.split(',')
+# 	actual_action = sp[-1]
+# 	payload = pay_load_format(i)
+# 	prediction = pred(payload)
 
-	if (prediction != actual_action):
-		wrong.append(idx)
-	# print(prediction == actual_action, actual_action, prediction)
+# 	if (prediction != actual_action):
+# 		wrong.append(idx)
+# 	# print(prediction == actual_action, actual_action, prediction)
 
 
 
-print(wrong)
+# print(wrong)
 
-print("Accuracy: {}%".format(accuracy))
+# print("Accuracy: {}%".format(accuracy))
